a35ddcd17a237ffa088b11b4f51b424e241a207d
RadmanRAZ <radmana92@gmail.com>
2024-08-10 19:11:28 +0330

    home page part 1

--- app/index.jsx
+++ app/index.jsx
@@ -2,16 +2,45 @@ import { View, Text, Image, ScrollView } from "react-native";
 import { Stack } from "expo-router";
 import { StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { useFonts } from "expo-font";
+import { images } from "../constants";
+import CustomBtn from "../components/CustomBtn";
 
 const Welcome = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-pblack">Home Page</Text>
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to tabs
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center  items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[148px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              resizeMode="contain"
+              className="absolute w-[80px] h-[15px] -bottom-2 -right-4"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            where creativity meets innovation : embark on a journey of limitless
+            exploration with Aora
+          </Text>
+          <CustomBtn />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
---
+++ components/CustomBtn.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export class CustomBtn extends Component {
+  render() {
+    return (
+      <View>
+        <Text>CustomBtn</Text>
+      </View>
+    );
+  }
+}
+
+export default CustomBtn;
